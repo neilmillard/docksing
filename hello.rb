14c912ae75df72cf6c865518e2d56dfd2a48e200
Neil Millard <neil.millard@moneysupermarket.com>
2016-04-18 17:27:43 +0100

    views and css

--- hello.rb
+++ hello.rb
@@ -3,21 +3,27 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 
 get '/' do
+  @title = 'Hello World'
   "Hello World"
 end
 
 get '/hello/:name' do
+   @title = 'Hello You'
   "Hello #{params[:name]}".strip
 end
 
 get '/form' do
+  @title = 'The Form'
   erb :form
 end
 
 post '/form' do
-  "You said '#{params[:message]}'"
+  @title = 'The Form'
+  @message = "You said '#{params[:content]}'"
+  erb :home
 end
 
 not_found do
+  @title = 'Page not found'
   halt 404, 'page not found'
 end

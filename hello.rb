fb151eef7b24d666ad6fbe8451ef7be5511edc9e
Neil Millard <neil.millard@moneysupermarket.com>
2016-04-18 16:59:35 +0100

    singing-with-sinatra

--- hello.rb
+++ hello.rb
@@ -3,5 +3,21 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 
 get '/' do
-  "Hello World #{params[:name]}".strip
+  "Hello World"
+end
+
+get '/hello/:name' do
+  "Hello #{params[:name]}".strip
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+not_found do
+  halt 404, 'page not found'
 end
